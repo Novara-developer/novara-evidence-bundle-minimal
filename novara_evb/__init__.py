1c72a9899a406f27cce036337a1a95823102c953
Novara-developer <skun.jp3312@gmail.com>
2025-11-23 06:08:43 +0800

    Initialize novara_evb library
    Initial creation of the novara_evb library with EvidenceBundle.

---
+++ novara_evb/__init__.py
@@ -0,0 +1,9 @@
+"""
+novara_evb
+
+Minimal helper library for creating Novara Evidence Bundles (v0.1).
+"""
+
+from .bundle import EvidenceBundle  # re-export for convenience
+
+__all__ = ["EvidenceBundle"]
